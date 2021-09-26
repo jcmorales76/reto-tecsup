a2f3b5444aa9552a318f5b4907587b8a7b11917b
Juan Carlos Morales R <43224641+jcmorales76@users.noreply.github.com>
2021-09-26 11:06:17 -0500

    Alert en mensajes finales

--- semana5/reto2/main.js
+++ semana5/reto2/main.js
@@ -1,4 +1,4 @@
-let Numero_hamburguesa = prompt("Ingresa la cantidad de Hamburguesas")
+let Numero_hamburguesa = +prompt("Ingresa la cantidad de Hamburguesas")
 console.log(Numero_hamburguesa," Unidad(es) de Hamburguesa")
 
 let tipo_hamburguesa = prompt("Seleccione el tipo de hamburguesa, 1. Simple ($20.00), 2. Doble ($25.00), 3. Triple ($28.00)")
@@ -7,16 +7,16 @@ let tburguer=tipo_hamburguesa;
 switch(tburguer){
     case "1":
         console.log("Selecciono la Hamburguesa Simple")
-        break
+        break;
     case "2":
         console.log("Selecciono la Hamburguesa Doble")
-        break
+        break;
     case "3":
         console.log("Selecciono la Hamburguesa Triple")
-        break
+        break;
     default:
         console.log("Usted escogio un opcion incorrecta") 
-        break
+        break;
 }
 
 if (tburguer == "1"){
@@ -33,16 +33,19 @@ if (tburguer == "1"){
 let tipo_pago = prompt("Seleccione el tipo de Pago: 1. Pago en Efectivo, 2. Pago con tarjeta de Crédito(Recargo de 5%)")
 switch(tipo_pago){
     case "1":
-        console.log("Selecciono Pago en Efectivo ");
-        console.log("Total a pagar  = ",total);
-        break
+        alert(`Cantidad de Hamburguesas ${Numero_hamburguesa}
+        Tipo de Hamburgues ${tburguer}
+        Total a pagar = ${total}`);
+        break;
     case "2":
-        console.log("Selecciono Pago con Tarjeta de Crédito")
-        console.log("Total a pagar = ",(total*1.05));
-        break
+        alert(`Cantidad de Hamburguesas ${Numero_hamburguesa}
+        Tipo de Hamburgues ${tburguer}
+        pagara con Tarjeta de Credito,Tiene un recargo del 5%
+        Total a pagar = ${total*1.05}`);
+        break;
     default:
         console.log("Seleccione una opcion correcta")
-        break
+        break;
 }
 
 
